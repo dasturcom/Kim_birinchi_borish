f9456db92b141baf47d3bc82f633a0222d2aca46
dasturcom <abdujabborshavkatjonov@gmail.com>
2022-09-14 09:18:01 +0500

    JavaScript

--- js/main.js
+++ js/main.js
@@ -18,9 +18,9 @@ var elCarMin = document.querySelector(".card-car-min");
 var elCarSecund = document.querySelector(".card-car-secund");
 
 //  Airplane  //
-var elAirplaneNum = document.querySelectorAll(".card-airplane-number");
-var elAirplaneMin = document.querySelectorAll(".card-airplane-min");
-var elAirplaneSecund = document.querySelectorAll(".card-airplane-secund");
+var elAirplaneNum = document.querySelector(".card-airplane-number");
+var elAirplaneMin = document.querySelector(".card-airplane-min");
+var elAirplaneSecund = document.querySelector(".card-airplane-secund");
 
 elBoxForm.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -28,47 +28,51 @@ elBoxForm.addEventListener("submit", (e) => {
     var boxInputValue = elBoxInput.value;
 
     var soat = (boxInputValue / 3.6).toFixed(2);
-    var soatYahlit = soat - Math.floor(soat).toFixed();
-    var minut = (60 * soatYahlit % 60).toFixed(2)
-    var secund = (60 * minut % 60).toFixed()
+    var soatYahlit = soat - Math.floor(soat).toFixed(2);
+    var minut = 60 * soatYahlit;
+    var minYahlit = minut - Math.floor(minut).toFixed(2);
+    var secund = 60 * minYahlit;
 
     //  People  //
-    elNumber.textContent = soat;
-    elMinut.textContent = minut;
-    elSecond.textContent = secund
+    elNumber.textContent = Math.floor(soat);
+    elMinut.textContent = Math.floor(minut);
+    elSecond.textContent = Math.floor(secund);
 
     // Bicycle //
     var bicycleSoat = (boxInputValue / 20.1).toFixed(2);
-    var bicycleSoatYahlit = bicycleSoat - Math.floor(bicycleSoat).toFixed();
-    var bicycleMinut = (60 * bicycleSoatYahlit % 60).toFixed(2)
-    var bicycleSecund = (60 * bicycleMinut % 60).toFixed()
+    var bicycleSoatYahlit = bicycleSoat - Math.floor(bicycleSoat).toFixed(2);
+    var bicycleMinut = 60 * bicycleSoatYahlit;
+    var bicycleMinYahlit = bicycleMinut - Math.floor(bicycleMinut).toFixed(2);
+    var bicycleSecund = 60 * bicycleMinYahlit;
 
     //  Bicycle  //
-    elBicycleNum.textContent = bicycleSoat;
-    elBicycleMun.textContent = bicycleMinut;
-    elBicycleSecund.textContent = bicycleSecund;
+    elBicycleNum.textContent = Math.floor(bicycleSoat);
+    elBicycleMun.textContent = Math.floor(bicycleMinut);
+    elBicycleSecund.textContent = Math.floor(bicycleSecund);
 
     // car //
     var carSoat = (boxInputValue / 70).toFixed(2);
-    var carSoatYahlit = carSoat - Math.floor(carSoat).toFixed();
-    var carMinut = (60 * carSoatYahlit % 60).toFixed(2);
-    var carSecund = (60 * carMinut % 60).toFixed();
+    var carSoatYahlit = carSoat - Math.floor(carSoat).toFixed(2);
+    var carMinut = 60 * carSoatYahlit;
+    var carMinYahlit = carMinut - Math.floor(carMinut).toFixed(2);
+    var carSecund = 60 * carMinYahlit;
 
     //  car  //
-    elCarNum.textContent = carSoat;
-    elCarMin.textContent = carMinut;
-    elCarSecund.textContent = carSecund;
+    elCarNum.textContent = Math.floor(carSoat);
+    elCarMin.textContent = Math.floor(carMinut);
+    elCarSecund.textContent = Math.floor(carSecund);
 
     // Airplane //
-    var airplaneSoat = (boxInputValue / 80).toFixed(2);
-    var airplaneSoatYahlit = airplaneSoat - Math.floor(airplaneSoat).toFixed();
-    var airplaneMinut = (60 * airplaneSoatYahlit % 60).toFixed(2);
-    var airplaneSecund = (60 * airplaneMinut % 60).toFixed();
+    var airplaneSoat = (boxInputValue / 800).toFixed(2);
+    var airplaneSoatYahlit = airplaneSoat - Math.floor(airplaneSoat).toFixed(2);
+    var airplaneMinut = 60 * airplaneSoatYahlit;
+    var airplaneMinYahlit = airplaneMinut - Math.floor(airplaneMinut).toFixed(2);
+    var airplaneSecund = 60 * airplaneMinYahlit;
 
     //  Airplane  //
-    elAirplaneNum.textContent = airplaneSoat;
-    elAirplaneMin.textContent = airplaneMinut;
-    elAirplaneSecund.textContent = airplaneSecund;
+    elAirplaneNum.textContent = Math.floor(airplaneSoat);
+    elAirplaneMin.textContent = Math.floor(airplaneMinut);
+    elAirplaneSecund.textContent = Math.floor(airplaneSecund); 
 })
 
 
